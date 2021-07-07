7888368aec4a822e67774afe84f160f82f485ec1
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-07-07 13:17:21 +0200

    feat: add package.json & dependencies

---
+++ js/layouts/en.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        small:'CapsLock',
+        shift:null,
+        code:'CapsLock',
+    },
+    {
+        small: '',
+        shift:'',
+        code:'Backquote',
+    },
+    {
+        small: '',
+        shift:'',
+        code:'',
+    },
+    {
+        small: '',
+        shift:'',
+        code:'',
+    },
+]
