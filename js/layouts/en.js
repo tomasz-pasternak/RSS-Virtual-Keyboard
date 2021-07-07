fec4e771c4dcb1d26236c4be64282b2d192cd6d1
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-07-07 15:11:26 +0200

    feat: create basic leter & numbers in english keyboard adjancies

--- js/layouts/en.js
+++ js/layouts/en.js
@@ -1,22 +1,212 @@
 export default [
     {
-        small: 'CapsLock',
-        shift: null,
-        code: 'CapsLock',
-    },
-    {
-        small: '',
-        shift: '',
+        small: '`',
+        shift: '~',
         code: 'Backquote',
     },
     {
-        small: '',
-        shift:'',
-        code:'',
+        small: '1',
+        shift: '!',
+        code: 'Digit1',
     },
     {
-        small: '',
-        shift:'',
-        code:'',
+        small: '2',
+        shift: '@',
+        code: 'Digit2',
+    },
+    {
+        small: '3',
+        shift: '#',
+        code: 'Digit3',
+    },
+    {
+        small: '4',
+        shift:'$',
+        code:'Digit4',
+    },
+    {
+        small: '5',
+        shift: '%',
+        code: 'Digit5',
+    },
+    {
+        small: '6',
+        shift: '^',
+        code: 'Digit6',
+    },
+    {
+        small: '7',
+        shift: '&',
+        code: 'Digit7',
+    },
+    {
+        small: '8',
+        shift: '*',
+        code: 'Digit8',
+    },
+    {
+        small: '9',
+        shift: '(',
+        code: 'Digit9',
+    },
+    {
+        small: '0',
+        shift: ')',
+        code: 'Digit9',
+    },
+    {
+        small: '-',
+        shift: '_',
+        code: 'Minus',
+    },
+    {
+        small: '+',
+        shift: '+',
+        code: 'Equal',
+    },
+    {
+        small: 'Backspace',
+        shift: null,
+        code: 'Backspace',
+    },
+    {
+        small: 'Delete',
+        shift: null,
+        code: 'Delete',
+    },
+    {
+        small: 'Tab',
+        shift: null,
+        code: 'Tab',
+    },
+    {
+        small: 'q',
+        shift: 'Q',
+        code: 'KeyQ',
+    },
+    {
+        small: 'w',
+        shift: 'W',
+        code: 'KeyW',
+    },
+    {
+        small: 'e',
+        shift: 'E',
+        code: 'KeyE',
+    },
+    {
+        small: 'r',
+        shift: 'R',
+        code: 'KeyR',
+    },
+    {
+        small: 't',
+        shift: 'T',
+        code: 'KeyT',
+    },
+    {
+        small: 'y',
+        shift: 'Y',
+        code: 'KeyY',
+    },
+    {
+        small: 'u',
+        shift: 'U',
+        code: 'KeyU',
+    },
+    {
+        small: 'i',
+        shift: 'I',
+        code: 'KeyI',
+    },
+    {
+        small: 'o',
+        shift: 'O',
+        code: 'KeyO',
+    },
+    {
+        small: 'P',
+        shift: 'P',
+        code: 'KeyP',
+    },
+    {
+        small: 'a',
+        shift: 'A',
+        code: 'KeyA',
+    },
+    {
+        small: 's',
+        shift: 'S',
+        code: 'KeyS',
+    },
+    {
+        small: 'd',
+        shift: 'D',
+        code: 'KeyD',
+    },
+    {
+        small: 'f',
+        shift: 'F',
+        code: 'KeyF',
+    },
+    {
+        small: 'g',
+        shift: 'G',
+        code: 'KeyG',
+    },
+    {
+        small: 'h',
+        shift: 'H',
+        code: 'KeyH',
+    },
+    {
+        small: 'j',
+        shift: 'J',
+        code: 'KeyJ',
+    },
+    {
+        small: 'k',
+        shift: 'K',
+        code: 'KeyK',
+    },
+    {
+        small: 'l',
+        shift: 'L',
+        code: 'KeyL',
+    },
+    {
+        small: 'z',
+        shift: 'Z',
+        code: 'KeyZ',
+    },
+    {
+        small: 'x',
+        shift: 'X',
+        code: 'KeyX',
+    },
+    {
+        small: 'c',
+        shift: 'C',
+        code: 'KeyC',
+    },
+    {
+        small: 'v',
+        shift: 'V',
+        code: 'KeyV',
+    },
+    {
+        small: 'b',
+        shift: 'B',
+        code: 'KeyB',
+    },
+    {
+        small: 'n',
+        shift: 'N',
+        code: 'KeyN',
+    },
+    {
+        small: 'm',
+        shift: 'M',
+        code: 'KeyM',
     },
 ]
