9ec5f867e78d26241c1637c9c5dcb53ba6d9bdc3
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-07-08 14:28:02 +0200

    feat: create local storage functionality

---
+++ js/storage.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+export function set(name, value) {
+  window.localStorage.setItem(name, JSON.stringify(value));
+}
+export function get(name, subst = null) {
+  return JSON.parse(window.localStorage.getItem(name) || subst);
+}
+export function remove(name) {
+  window.localStorage.removeItem(name);
+}
