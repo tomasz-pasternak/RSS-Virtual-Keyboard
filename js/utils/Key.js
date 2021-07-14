371e5fa1083c5251fc0a18e7d860459ceca84786
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-07-14 14:41:19 +0200

    feat:  create Key class with methods

---
+++ js/utils/Key.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/extensions */
+import create from './utils/create.js';
+
+export default class Key {
+  constructor({ small, shift, code }) {
+    this.small = small;
+    this.shift = shift;
+    this.code = code;
+    this.isFnKey = Boolean(small.match(/Ctrl|arr|Alt|Shift|Tab|Back|Del|Enter|Caps|Space|Win/));
+
+    if (shift && shift.match(/[^a-zA-Z0-9]/)) {
+      this.sub = create('div', 'sub', this.shift);
+    } else {
+      this.sub = create('div', 'sub', '');
+    }
+
+    this.letter = create('div', 'letter', small);
+    this.div = create('div', 'keyboard_key', [this.sub, this.letter], null, ['code', this.code]);
+  }
+}
+
+// new Key()
