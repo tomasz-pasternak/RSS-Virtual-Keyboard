500d3e113a651fd0f8599ca3e7f7679382732e87
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-08-02 13:27:53 +0200

    feat: add KeyboardJS basic Keyboard template of english keys  rendering

--- js/Keyboard.js
+++ js/Keyboard.js
@@ -1,4 +1,46 @@
-/* eslint-disable import/extensions */
 /* eslint-disable no-param-reassign */
+/* eslint-disable no-plusplus */
+/* eslint-disable import/extensions */
+import * as storage from './Storage.js';
+import create from './utils/create.js';
+import language from './layouts/index.js';
+import Key from './Key.js';
 
-import
+const main = create('main', '', [create('h1', 'title', 'RSS Virtual Keyboard')]);
+
+export default class Keyboard {
+  constructor(rowsOrder) {
+    this.rowsOrder = rowsOrder;
+    this.keysPressed = {};
+    this.isCaps = false;
+  }
+
+  init(code) {
+    this.keyBase = language[code];
+    this.output = create('textarea', 'output', null, main,
+      ['placeholder', 'Start typing...)'],
+      ['rows', 5],
+      ['cols', 50],
+      ['spellcheck', false],
+      ['autocorrect', 'off']);
+    this.container = create('div', 'keyboard', null, main, ['language', code]);
+    document.body.prepend(main);
+    return this;
+  }
+
+  generateLayout() {
+    this.keyButtons = [];
+    this.rowsOrder.forEach((row, i) => {
+      const rowElement = create('div', 'keyboard__row', null, this.container, ['row', i + 1]);
+      rowElement.style.gridTemplateColumns = `repeat(${row.length}, 1fr)`;
+      row.forEach((code) => {
+        const keyObj = this.keyBase.find((key) => key.code === code);
+        if (keyObj) {
+          const keyButton = new Key(keyObj);
+          this.keyButtons.push(keyButton);
+          rowElement.appendChild(keyButton.div);
+        }
+      });
+    });
+  }
+}
--- js/utils/create.js
+++ js/utils/create.js
@@ -1,37 +1,40 @@
-/* eslint-disable linebreak-style */
 /**
- *@param {String} el
+ * @param {HTMLElement} el
  * @param {String} classNames
  * @param {HTMLElement} child
  * @param {HTMLElement} parent
- * @param {...array} dataAttr
-*/
-
-export default function create(el, classNames, child, parent, ...dataAttr) {
+ * @param  {...any} dataAttr
+ */
+export default function create(el, classNames = '', child, parent, ...dataAttr) {
+  // Create HTMLElement
   let element = null;
   try {
     element = document.createElement(el);
   } catch (error) {
-    throw new Error('Unable to create HTML element!Give a propper tag name');
+    throw new Error('Unable to create HTMLElement! You should give a proper HTML tag name');
   }
-  if (classNames) element.classList.add(...classNames.split(' ')); // 'class1' , 'class2'
+
+  // split and apply classNames
+  if (classNames) element.classList.add(...classNames.split(' '));
+
   if (child && Array.isArray(child)) {
     child.forEach((childElement) => childElement && element.appendChild(childElement));
   } else if (child && typeof child === 'object') {
     element.appendChild(child);
-  } else if (child && typeof child === 'string') {
+  } else if (typeof child === 'string') {
     element.innerHTML = child;
   }
-
   if (parent) {
     parent.appendChild(element);
   }
+
   if (dataAttr.length) {
     dataAttr.forEach(([attrName, attrValue]) => {
       if (attrValue === '') {
         element.setAttribute(attrName, '');
+        return;
       }
-      if (attrName.math(/value|id|placeholder|cols|rows|autocorrect|spellcheck/)) {
+      if (attrName.match(/value|id|placeholder|cols|rows|autocorrect|spellcheck/i)) {
         element.setAttribute(attrName, attrValue);
       } else {
         element.dataset[attrName] = attrValue;
