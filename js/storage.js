9249cf9c8a83294f0e164e779186cb723dc26c47
Rostyslav Lohvynov <rostyslav_lohvynov@sevenet.pl>
2021-08-08 09:11:01 +0200

    fix : some corrections

--- js/storage.js
+++ js/storage.js
@@ -5,6 +5,7 @@ export function set(name, value) {
 export function get(name, subst = null) {
   return JSON.parse(window.localStorage.getItem(name) || subst);
 }
+// remove item from local storage
 export function remove(name) {
   window.localStorage.removeItem(name);
 }
--- js/utils/create.js
+++ js/utils/create.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /**
  * @param {HTMLElement} el
  * @param {String} classNames
